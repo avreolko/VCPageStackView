bf9e4d1bef16b73836d203355cddda8d59fc76b8
Валентин Черепянко <vacherepyanko@sberbank.ru>
2021-05-04 11:15:10 +0300

    add: license

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "VCPageStackView",
-            targets: ["VCPageStackView"]),
+            targets: ["VCPageStackView"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
